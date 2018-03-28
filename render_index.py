e20288860607b0761a249fcb67da9f7677a967dd
Ian Dees <ian.dees@gmail.com>
2018-03-27 23:18:36 -0500

    List the S3 bucket and build an HTML page for its contents

--- render_index.py
+++ render_index.py
@@ -1,9 +1,21 @@
+import boto3
 import json
 import os
 from collections import defaultdict
 from jinja2 import Environment, FileSystemLoader
 
 THIS_DIR = os.path.dirname(os.path.abspath(__file__))
+KEY_PATTERNS = {
+    "imposm-geojson.zip": "Imposm GeoJSON",
+    "imposm-shapefiles.zip": "Imposm Shapefiles",
+    "land.coastline.zip": "Land Coastline",
+    "water.coastline.zip": "Water Coastline",
+    "osm.bz2": "Raw OSM (bz2 compressed)",
+    "osm.pbf": "Raw OSM (as PBF)",
+    "osm2pgsql-geojson.zip": "osm2pgsql GeoJSON",
+    "osm2pgsql-shapefiles.zip": "osm2pgsql Shapefiles",
+}
+
 
 def render_html():
     j2_env = Environment(loader=FileSystemLoader(THIS_DIR),
@@ -12,6 +24,28 @@ def render_html():
     city_features = []
     with open("cities.geojson", "r") as f:
         city_features = json.load(f).get('features')
+
+    # List the S3 bucket and pull in the file size + timestamps
+    client = boto3.client('s3')
+    paginator = client.get_paginator('list_objects_v2')
+    response_iterator = paginator.paginate(
+        Bucket='metro-extracts.nextzen.org',
+        Delimiter='/',
+    )
+    file_infos = defaultdict(lambda: defaultdict(dict))
+    for page in response_iterator:
+        for obj in page['Contents']:
+            k = obj['Key']
+            try:
+                metro_id, filetype = k.split('.', 1)
+            except ValueError:
+                continue
+
+            file_infos[metro_id][filetype] = {
+                'last_modified': obj['LastModified'],
+                'etag': obj['ETag'],
+                'size': obj['Size'],
+            }
 
     feature_tree = defaultdict(lambda: defaultdict(dict))
     for f in city_features:
@@ -23,6 +57,8 @@ def render_html():
 
     return j2_env.get_template('map_template.html').render(
         feature_tree=feature_tree,
+        file_infos=file_infos,
+        file_type_infos=KEY_PATTERNS,
     )
 
 if __name__ == '__main__':
