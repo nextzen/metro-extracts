f6a177064cea8ca5420c2861ed240dac19a59668
Ian Dees <ian.dees@gmail.com>
2018-05-19 09:53:18 -0500

    Generate index.html based on the GeoJSON manifest that is generated

--- render_index.py
+++ render_index.py
@@ -1,53 +1,27 @@
-import argparse
-import boto3
 import json
 import os
+import sys
 from collections import defaultdict
+from datetime import datetime
 from jinja2 import Environment, FileSystemLoader
+
+# Pipe a geojson index created by `generate_geojson_index.py` to this script
+# and it will write an HTML index doc back to stdout.
 
 THIS_DIR = os.path.dirname(os.path.abspath(__file__))
 KEY_PATTERNS = {
-    "imposm-geojson.zip": "Imposm GeoJSON",
-    "imposm-shapefiles.zip": "Imposm Shapefiles",
-    "land.coastline.zip": "Land Coastline",
-    "water.coastline.zip": "Water Coastline",
-    "osm.bz2": "Raw OSM (bz2 compressed)",
-    "osm.pbf": "Raw OSM (as PBF)",
-    "osm2pgsql-geojson.zip": "osm2pgsql GeoJSON",
+    "imposm-geojson.zip":       "Imposm GeoJSON",
+    "imposm-shapefiles.zip":    "Imposm Shapefiles",
+    "land.coastline.zip":       "Land Coastline",
+    "water.coastline.zip":      "Water Coastline",
+    "osm.bz2":                  "Raw OSM (bz2 compressed)",
+    "osm.pbf":                  "Raw OSM (as PBF)",
+    "osm2pgsql-geojson.zip":    "osm2pgsql GeoJSON",
     "osm2pgsql-shapefiles.zip": "osm2pgsql Shapefiles",
 }
 
-
-def render_html(s3_prefix=None):
-    j2_env = Environment(loader=FileSystemLoader(THIS_DIR),
-                         trim_blocks=True)
-
-    city_features = []
-    with open(os.path.join(THIS_DIR, "cities.geojson"), "r") as f:
-        city_features = json.load(f).get('features')
-
-    # List the S3 bucket and pull in the file size + timestamps
-    client = boto3.client('s3')
-    paginator = client.get_paginator('list_objects_v2')
-    response_iterator = paginator.paginate(
-        Bucket='metro-extracts.nextzen.org',
-        Prefix=s3_prefix,
-        Delimiter='/',
-    )
-    file_infos = defaultdict(lambda: defaultdict(dict))
-    for page in response_iterator:
-        for obj in page['Contents']:
-            k = obj['Key']
-            try:
-                metro_id, filetype = k.split('.', 1)
-            except ValueError:
-                continue
-
-            file_infos[metro_id][filetype] = {
-                'last_modified': obj['LastModified'],
-                'etag': obj['ETag'],
-                'size': obj['Size'],
-            }
+def render_html(data):
+    city_features = data.get('features')
 
     feature_tree = defaultdict(lambda: defaultdict(dict))
     for f in city_features:
@@ -57,17 +31,17 @@ def render_html(s3_prefix=None):
 
         feature_tree[region_name][country_name][metro_name] = f
 
+    j2_env = Environment(loader=FileSystemLoader(THIS_DIR),
+                         trim_blocks=True)
+
     return j2_env.get_template('map_template.html').render(
+        planet_timestamp=data.get('planet_timestamp'),
         feature_tree=feature_tree,
-        file_infos=file_infos,
         file_type_infos=KEY_PATTERNS,
-        s3_prefix=s3_prefix or '',
     )
 
 if __name__ == '__main__':
-    parser = argparse.ArgumentParser()
-    parser.add_argument('--prefix', help="The S3 prefix to base the index.html on")
-    args = parser.parse_args()
 
-    with open('index.html', 'w') as f:
-        f.write(render_html(args.prefix))
+    data = json.load(sys.stdin)
+
+    sys.stdout.write(render_html(data))
